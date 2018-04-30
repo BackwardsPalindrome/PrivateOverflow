9396e4596d79e6a00a43b1478094021a120185c9
Maximilien Monteil <maximilienmonteil@gmail.com>
2018-04-30 19:33:32 +0300

    flesh out /java routes

--- api/routes/java.js
+++ api/routes/java.js
@@ -3,7 +3,42 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
-    message: 'Hit the /java GET route'
+    message: 'Handling GET requests to /java'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  const snippet = {
+    title: req.body.title,
+    code: req.body.code
+  }
+  res.status(201).json({
+    message: 'Handling POST requests to /java',
+    snippet: snippet
+  })
+})
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  res.status(200).json({
+    message: 'Handling GET requests to /java/:id',
+    id: id
+  })
+})
+
+router.patch('/:id', (req, res, next) => {
+  const id = req.params.id
+  res.status(200).json({
+    message: 'Handling PATCH requests to /java/:id',
+    id: id
+  })
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  res.status(200).json({
+    message: 'Handling DELETE requests to /java/:id',
+    id: id
   })
 })
 
